f86f281f1ebb03127f835f86c819ce95944b73e3
brouwerb <benedictbrouwer@gmail.com>
2022-11-07 23:39:51 +0100

    l

--- week_3/ludecomp.c
+++ week_3/ludecomp.c
@@ -75,29 +75,119 @@ void extract(int len, double LU[len][len], double L[len][len], double U[len][len
     
 }
 
+void solve(int len, double L[len][len], double U[len][len], double b[len], double x[len]){
+    double y[len];
+    for (int i = 0; i < len; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < i; j++)
+        {
+            sum += L[i][j]*y[j];
+        }
+        y[i] = b[i] - sum;
+    }
+    for (int i = len-1; i >= 0; i--)
+    {
+        double sum = 0;
+        for (int j = i+1; j < len; j++)
+        {
+            sum += U[i][j]*x[j];
+        }
+        x[i] = (y[i] - sum)/U[i][i];
+    }
+    
+}
+
+void copy(int len, double A[len][len], double B[len][len]){
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            B[i][j] = A[i][j];
+        }
+        
+    }
+    
+}
+
+int check(int len, double A[len][len], double L[len][len], double U[len][len]){
+    double res[len][len];
+    multiply(len, L, U, res);
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            if (res[i][j] != A[i][j])
+            {
+                return 0;
+            }
+            
+        }
+        
+    }
+    return 1;
+    
+}
+void fillzero(int len, double A[len][len]){
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            A[i][j] = 0;
+        }
+        
+    }
+    
+}
+
+void calculate(int len, double A[len][len], double b[len], double x[len]){
+    printf("Matrix: \n");
+    printmatrix(len, A);
+    double LU[len][len];
+    copy(len, A, LU);
+    decomp(len, LU);
+    double L[len][len];
+    double U[len][len];
+    double D[len][len];
+    fillzero(len, D);
+    fillzero(len, L);
+    fillzero(len, U);
+    extract(len, LU, L, U);
+    printf("L: \n");
+    printmatrix(len, L);
+    printf("U: \n");
+    printmatrix(len, U);
+    multiply(len, L, U, D);
+    printf("D: \n");
+    printmatrix(len, D);
+    printf("%i", check(len, A, L, U));
+    if(check(len, A, L, U)){
+        printf("L:\n");
+        printmatrix(len, L);
+        printf("U:\n");
+        printmatrix(len, U);
+        solve(len, L, U, b, x);
+        printVector(len, x);
+    }
+    else{
+        printf("Error");
+    }
+
+}
+
+
 int main(){
 
 	double x[3];
 
 	double A[3][3] = {{4, 3, -2}, {-1, -1, 3}, {2, -1, 5}};
-    double LU[3][3];
-	const double b[] = {9, -4, 6};
-    double L[3][3];
-    double U[3][3];
-    printmatrix(3, A);
-	decomp(3, A);
-	printmatrix(3, A);
-    extract(3, A, L, U);
-    printmatrix(3, L);
-    printmatrix(3, U);
-	// printVector(3, x);
 
-	// double B[][3] = {{0, 3, -2}, {-1, -1, 3}, {2, -1, 5}};
-	// const double c[] = {-7, -4, 6};
-	// // TODO: implement your solver
-	// printmatrix(3, 3, B);
-	// printVector(3, x);
+	double b[] = {9, -4, 6};
+    calculate(3, A, b, x);
 
+	double B[3][3] = {{0, 3, -2}, {-1, -1, 3}, {2, -1, 5}};
+	double c[3] = {-7, -4, 6};
+    calculate(3, B, c, x);
 
 	return 0;
 }
